af0f0d58b6f22c71b8639554f951989633ced0b9
MarsIfeanyi <marschalice@gmail.com>
2023-06-10 16:47:35 +0100

    Initial commit

---
+++ 01-HelloWorld/hello_cargo/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+     println!("Everything is Learnable");
+    println!("0xMars lets get rusty")
+}
---
+++ 01-HelloWorld/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/marsifeanyi/<>/Rust\ Programming/hello_cargo/target/debug/hello_cargo: /home/marsifeanyi/<>/Rust\ Programming/hello_cargo/src/main.rs
