ae0625814ef10d461add8e5b37209aa7d3ccf4a5
MarsIfeanyi <marschalice@gmail.com>
2023-12-26 21:10:17 +0100

    Declaring Variables in Rust

--- 03-CommonProgrammingConcept/common_programming_concepts/src/main.rs
+++ 03-CommonProgrammingConcept/common_programming_concepts/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+
+// VARIABLES
+
+    // In Rust variables are immutable by default
+
+    let mut  x: i32 = 5;
+    println!("The value of x is: {}", x);
+
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    // Declaring Constant variables. Hint: You cannot mutate a constant
+
+    const SUBSCRIBER_COUNT:u32 = 100_000;
+
+    // SHADOWING: It allows you to create a new variable, using an existing name.
+let   y: i32 = 5;
+    println!("The value of y is: {}", y);
+
+    // Hint: The first y variable is shadowed by the 2nd y variable. 
+  let   y:&str ="Six" ;
+    println!("The value of y is: {}", y);
+    
 }
